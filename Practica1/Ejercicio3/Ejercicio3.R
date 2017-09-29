0a8fd3c0c82c997ec4fd51ad7c80e57e25bb56cb
Kriss-Gutierrez-Anco <kriss-lady@hotmail.com>
2017-09-29 13:49:14 -0500

    Archivos para Practica1

--- Practica1/Ejercicio3/Ejercicio3.R
+++ Practica1/Ejercicio3/Ejercicio3.R
@@ -1,7 +1,5 @@
 #Pregunta 3
 #casos de elegir a seis niños importando el orden permutacion
-p<- c("niño1","niño2","niño3","niño4","niño5","niño6")
-#casos de elegir a seis niños por orden solo hay un caso:
-
+1/(cumprod(1:6)[6])#Probabilidad 
 #Hay tres muchachos y tres muchachas, de que las tres muchachas sean primeras
 1/combn(1:6,3)
