674d6fab05309d999ea86066f4c4fc46ff76cc7d
Kriss-Gutierrez-Anco <kriss-lady@hotmail.com>
2017-09-29 13:50:10 -0500

    Archivos para Practica1

--- Practica1/Ejercicio4/Ejercicio4.R
+++ Practica1/Ejercicio4/Ejercicio4.R
@@ -1,9 +1,5 @@
 #Pregunta 4 
 #probabilidad(2^k>=15), ya que es discreto e independiente
 #que gane mas de 15 soles es cuando k>=4
-#que es el reciproco que cuando k<3, osea k=1,2,3 y estos casos son independientes
-#P(k)=1/(2^k)
-a<-1/(2^(1:3))
-1-sum(a)
 b<-1/(2^(1:1000))
 sum(b)
